deacf74093a0a177fa4508527e436c11597f7600
milppvc <mil.ppvc@gmail.com>
2017-08-28 20:01:05 +0200

    1st commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+var $btns = $('.btn').click(function() {
+  if (this.id == 'all') {
+    $('#parent > div').fadeIn(450);
+  } else {
+    var $el = $('.' + this.id).fadeIn(450);
+    $('#parent > div').not($el).hide();
+  }
+  $btns.removeClass('active');
+  $(this).addClass('active');
+})
+});
